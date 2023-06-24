65afc3002d9fbe7e946d42302194ceaa4ff0797b
danielpdwalker <danielpdwalker@gmail.com>
2023-06-24 09:36:45 +0100

    Initial tap template commit

---
+++ tests/test_core.py
@@ -0,0 +1,22 @@
+"""Tests standard tap features using the built-in SDK tests library."""
+
+import datetime
+
+from singer_sdk.testing import get_tap_test_class
+
+from tap_themeparks.tap import Tapthemeparks
+
+SAMPLE_CONFIG = {
+    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
+    # TODO: Initialize minimal tap config
+}
+
+
+# Run standard built-in tap tests from the SDK:
+TestTapthemeparks = get_tap_test_class(
+    tap_class=Tapthemeparks,
+    config=SAMPLE_CONFIG,
+)
+
+
+# TODO: Create additional tests as appropriate for your tap.
