0f50332e78c144dd207f491fde764d8eaa2be1df
danielpdwalker <danielpdwalker@gmail.com>
2023-06-24 11:35:16 +0100

    Added destinations stream. Removed unneeded code in client.py

--- tap_themeparks/streams.py
+++ tap_themeparks/streams.py
@@ -8,58 +8,30 @@ from singer_sdk import typing as th  # JSON Schema typing helpers
 
 from tap_themeparks.client import themeparksStream
 
-# TODO: Delete this is if not using json files for schema definition
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
-# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
-#       - Copy-paste as many times as needed to create multiple stream types.
 
+class DestinationsStream(themeparksStream):
+    """Define destinations stream"""
 
-class UsersStream(themeparksStream):
-    """Define custom stream."""
-
-    name = "users"
-    path = "/users"
+    name = "destinations"
+    path = "/destinations"
     primary_keys = ["id"]
     replication_key = None
-    # Optionally, you may also use `schema_filepath` in place of `schema`:
-    # schema_filepath = SCHEMAS_DIR / "users.json"  # noqa: ERA001
+    records_jsonpath = "$.destinations[*]"
+
     schema = th.PropertiesList(
-        th.Property("name", th.StringType),
         th.Property(
             "id",
             th.StringType,
-            description="The user's system ID",
+            description="The destination's id",
         ),
-        th.Property(
-            "age",
-            th.IntegerType,
-            description="The user's age in years",
-        ),
-        th.Property(
-            "email",
-            th.StringType,
-            description="The user's email address",
-        ),
-        th.Property("street", th.StringType),
-        th.Property("city", th.StringType),
-        th.Property(
-            "state",
-            th.StringType,
-            description="State name in ISO 3166-2 format",
-        ),
-        th.Property("zip", th.StringType),
-    ).to_dict()
-
-
-class GroupsStream(themeparksStream):
-    """Define custom stream."""
-
-    name = "groups"
-    path = "/groups"
-    primary_keys = ["id"]
-    replication_key = "modified"
-    schema = th.PropertiesList(
         th.Property("name", th.StringType),
-        th.Property("id", th.StringType),
-        th.Property("modified", th.DateTimeType),
+        th.Property("slug", th.StringType),
+        th.Property(
+            "parks",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("id", th.StringType), th.Property("name", th.StringType)
+                )
+            ),
+        ),
     ).to_dict()
--- tap_themeparks/tap.py
+++ tap_themeparks/tap.py
@@ -15,32 +15,7 @@ class Tapthemeparks(Tap):
     name = "tap-themeparks"
 
     # TODO: Update this section with the actual config values you expect:
-    config_jsonschema = th.PropertiesList(
-        th.Property(
-            "auth_token",
-            th.StringType,
-            required=True,
-            secret=True,  # Flag config as protected.
-            description="The token to authenticate against the API service",
-        ),
-        th.Property(
-            "project_ids",
-            th.ArrayType(th.StringType),
-            required=True,
-            description="Project IDs to replicate",
-        ),
-        th.Property(
-            "start_date",
-            th.DateTimeType,
-            description="The earliest record date to sync",
-        ),
-        th.Property(
-            "api_url",
-            th.StringType,
-            default="https://api.mysample.com",
-            description="The url for the API service",
-        ),
-    ).to_dict()
+    config_jsonschema = th.PropertiesList().to_dict()
 
     def discover_streams(self) -> list[streams.themeparksStream]:
         """Return a list of discovered streams.
@@ -49,8 +24,7 @@ class Tapthemeparks(Tap):
             A list of discovered streams.
         """
         return [
-            streams.GroupsStream(self),
-            streams.UsersStream(self),
+            streams.DestinationsStream(self),
         ]
 
 
