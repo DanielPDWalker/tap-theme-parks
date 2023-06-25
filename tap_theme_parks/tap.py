4f6f8a98fb0dccecd5dd9c731818b5a86146bc47
danielpdwalker <danielpdwalker@gmail.com>
2023-06-25 12:49:44 +0100

    Removed unneeded TODOs in tap.py

--- tap_theme_parks/tap.py
+++ tap_theme_parks/tap.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 from singer_sdk import Tap
 from singer_sdk import typing as th  # JSON schema typing helpers
 
-# TODO: Import your custom stream types here:
 from tap_theme_parks import streams
 
 
@@ -14,7 +13,6 @@ class TapThemeParks(Tap):
 
     name = "tap-theme-parks"
 
-    # TODO: Update this section with the actual config values you expect:
     config_jsonschema = th.PropertiesList(
         th.Property("live_data_array", th.ArrayType(th.StringType)),
     ).to_dict()
