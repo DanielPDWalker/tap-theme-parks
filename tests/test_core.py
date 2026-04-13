ebb958db4a860f6e68f3ab7f70a9efb164e7b0f2
Daniel Walker <danielpdwalker@gmail.com>
2026-04-13 22:29:40 +0100

    Limit the test_tap_stream_connections sdk test to only sync the one record as when running across multiple python version we were hitting API limits

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from singer_sdk.testing import get_tap_test_class
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
 
 from tap_theme_parks.tap import TapThemeParks
 
@@ -12,10 +12,20 @@ SAMPLE_CONFIG = {"live_data_array": ["1", "2", "3"]}
 
 
 # Run standard built-in tap tests from the SDK:
-TestTapThemeParks = get_tap_test_class(
-    tap_class=TapThemeParks,
-    config=DEFAULT_CONFIG,
-)
+class TestTapThemeParks(
+    get_tap_test_class(
+        tap_class=TapThemeParks,
+        config=DEFAULT_CONFIG,
+        suite_config=SuiteConfig(max_records_limit=1),
+    )
+):
+    def test_tap_stream_connections(self) -> None:
+        """Test connectivity using only the top-level stream to avoid rate limiting."""
+        tap = TapThemeParks(config=DEFAULT_CONFIG)
+        tap.run_sync_dry_run(
+            dry_run_record_limit=1,
+            streams=[tap.streams["destination"]],
+        )
 
 
 class TestEnabledStreams(unittest.TestCase):
